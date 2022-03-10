21189e033fd4b4f56414a1fcf980adc3e64f533e
JINJITING <jinjiting2690@gmail.com>
2022-03-10 10:34:14 +0800

    feat(c): update binary-search

---
+++ c/384/278.c
@@ -0,0 +1,14 @@
+#include <stdbool.h>
+
+// The API isBadVersion is defined for you.
+bool isBadVersion(int version);
+
+int firstBadVersion(int n) {
+  int l = 1, r = n, m;
+  while(l < r) {
+    m = l + (r - l) / 2;
+    if (isBadVersion(m)) r = m;
+    else l = m + 1;
+  }
+  return l;
+}
