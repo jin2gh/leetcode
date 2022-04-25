249218587c9ba070a2fdde88acc6aeda4f34c895
JINJITING <jinjiting2690@gmail.com>
2022-04-25 10:27:39 +0800

    feat(ts): update No.(167,1608)

--- ts/1664/1608.ts
+++ ts/1664/1608.ts
@@ -1,3 +1,4 @@
+// 计数 + 后缀和
 function specialArray(nums: number[]): number {
   const len = nums.length
   const cnts = new Array(len + 1).fill(0)
@@ -6,8 +7,28 @@ function specialArray(nums: number[]): number {
     else cnts[v] += 1
   }
   for (let i = len; i >= 0; --i) {
-    if (i < len)  cnts[i] += cnts[i + 1]
+    if (i < len) cnts[i] += cnts[i + 1]
     if (cnts[i] === i) return i
+  }
+  return -1
+};
+
+
+// 二分查找
+function specialArray1(nums: number[]): number {
+  let l = 1,
+      r = nums.length,
+      m = 0,
+      cnt = 0
+  while (l <= r) {
+    m = l + ((r - l) >> 1)
+    cnt = nums.reduce((cnt, val) => {
+      if (val >= m) cnt += 1
+      return cnt
+    }, 0)
+    if (m === cnt) return m
+    if (m > cnt) r = m - 1
+    else l = m + 1
   }
   return -1
 };
--- ts/256/167.ts
+++ ts/256/167.ts
@@ -1,12 +1,27 @@
+// 双指针
 function twoSum(numbers: number[], target: number): number[] {
   let l = 0, r = numbers.length - 1
   while (l < r) {
     let sum = numbers[l] + numbers[r]
     if (sum === target) return [l + 1, r + 1]
-    if (sum > target) {
-      r -= 1
-    } else {
-      l += 1
+    if (sum > target) r -= 1
+    else l += 1
+  }
+  return [-1, -1]
+};
+
+
+// 二分查找
+function twoSum1(numbers: number[], target: number): number[] {
+  const n = numbers.length
+  for (let i = 0; i < n; ++i) {
+    const tg = target - numbers[i]
+    let l = i + 1, r = n - 1, m = 0
+    while (l <= r) {
+      m = l + ((r - l) >> 1)
+      if (numbers[m] === tg) return [i + 1, m + 1]
+      if (numbers[m] < tg) l = m + 1
+      else r = m - 1
     }
   }
   return [-1, -1]
