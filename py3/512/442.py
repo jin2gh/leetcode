a123a4e6fcdeb8f32dab39dae6298655c1a9749f
JINJITING <jinjiting2690@gmail.com>
2022-05-08 23:41:59 +0800

    fix(py,ts): update No.442

---
+++ py3/512/442.py
@@ -0,0 +1,9 @@
+from typing import List
+
+class Solution:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        for i in range(len(nums)):
+            while nums[i] != nums[nums[i] - 1]:
+                nums[nums[i] - 1], nums[i] = nums[i], nums[nums[i] - 1]
+
+        return [v for i, v in enumerate(nums) if v - 1 != i]
